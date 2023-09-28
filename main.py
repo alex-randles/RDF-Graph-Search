9e6ebe2118f316a06aa7ce33008d8337fb327088
Alex Randles <alexrandles0@gmail.com>
2023-09-28 02:57:53 +0100

    backup

--- main.py
+++ main.py
@@ -21,49 +21,57 @@ def index():
 def search_graph():
     search_term = request.form.get("search_term")
     graph_uri = request.form.get("graph_uri")
-    # graph_uri = "https://dbpedia.org/sparql"
     print(search_term, graph_uri)
+    # VRTI Query
     sparql_query = f"""
     PREFIX crm: <http://erlangen-crm.org/current/>
-    SELECT DISTINCT ?person ?occupation
+    PREFIX vrti: <http://ont.virtualtreasury.ie/ontology#>
+    SELECT DISTINCT ?person ?occupation ?gender
     WHERE {{
       ?person ?predicate ?object; 
           crm:P1_is_identified_by ?name;
+           crm:P2_has_type  ?gender;
           a crm:E21_Person .
       ?occupation crm:P107_has_current_or_former_member ?person ;
                   rdfs:label ?occupationName   .
-      ?name rdfs:label ?nameLabel . 
-      FILTER CONTAINS(LCASE(?occupationName),'{search_term}')     
+      ?name rdfs:label ?nameLabel  ;
+            a   crm:E41_Appellation  . 
+      FILTER CONTAINS(LCASE(?nameLabel),'{search_term}')     
      }}
-    LIMIT 10000
+    LIMIT 100
     """
+    # DBpedia Query
+    sparql_query = f"""
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        SELECT ?athlete ?place
+        WHERE
+        {{
+          ?athlete  rdfs:label      ?label ;
+                    dbo:birthPlace  ?place .
+          ?place    a               dbo:City ;
+                    rdfs:label      ?cityName .
+                    FILTER (STR(?cityName) = '{search_term}')
+        }}
+        LIMIT 100
+    """
+    # Wikidata Query
     # sparql_query = f"""
-    #     PREFIX dbo: <http://dbpedia.org/ontology/>
-    #     SELECT ?athlete ?place
-    #     WHERE
-    #     {{
-    #       ?athlete  rdfs:label      ?label ;
-    #                 dbo:birthPlace  ?place .
-    #       ?place    a               dbo:City ;
-    #                 rdfs:label      ?cityName .
-    #                 FILTER (STR(?cityName) = '{search_term}')
-    #     }}
-    #     LIMIT 1000
+    # PREFIX wd: <http://www.wikidata.org/entity/>
+    # PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+    # SELECT DISTINCT ?item ?itemLabel ?occupationLabel
+    # WHERE
+    # {{
+    #       ?item wdt:P31 wd:Q5;
+    #           wdt:P19/wdt:P131* wd:Q60;
+    #           wdt:P27 wd:Q30;
+    #           wdt:P106 ?occupation;
+    #           rdfs:label ?itemLabel .
+    #      ?occupation wdt:P3321 ?occupationLabel  .
+    #      FILTER(LANG(?itemLabel) = "en" && LANG(?occupationLabel) = "en" && STR(?occupationLabel) = "{search_term}")
+    # }}
+    # LIMIT 10
     # """
-    sparql_query = """
-    SELECT DISTINCT ?item ?itemLabel ?occupationLabel
-    WHERE 
-    {
-          ?item wdt:P31 wd:Q5;         
-              wdt:P19/wdt:P131* wd:Q60; 
-              wdt:P27 wd:Q30; 
-              wdt:P106 ?occupation;
-              rdfs:label ?itemLabel . 
-         ?occupation wdt:P3321 ?occupationLabel  . 
-         FILTER(LANG(?itemLabel) = "en" && LANG(?occupationLabel) = "en")
-    }
-    LIMIT 1000
-    """
+    print(sparql_query)
     table_rows = {}
     try:
         sparql = SPARQLWrapper(graph_uri)
