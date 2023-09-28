36c79b50735f2a104d26a3485bd26b6c04abe725
Alex Randles <alexrandles0@gmail.com>
2023-09-28 14:27:58 +0100

    basic exception handling added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "x633UE2xYRC"
 
 @app.route('/', methods=["GET"])
 def index():
@@ -37,19 +37,19 @@ def search_graph():
     LIMIT 100
     """
     # DBpedia Query
-    sparql_query = f"""
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        SELECT ?athlete ?place
-        WHERE
-        {{
-          ?athlete  rdfs:label      ?label ;
-                    dbo:birthPlace  ?place .
-          ?place    a               dbo:City ;
-                    rdfs:label      ?cityName .
-                    FILTER (STR(?cityName) = '{search_term}')
-        }}
-        LIMIT 100
-    """
+    # sparql_query = f"""
+    #     PREFIX dbo: <http://dbpedia.org/ontology/>
+    #     SELECT ?athlete ?place
+    #     WHERE
+    #     {{
+    #       ?athlete  rdfs:label      ?label ;
+    #                 dbo:birthPlace  ?place .
+    #       ?place    a               dbo:City ;
+    #                 rdfs:label      ?cityName .
+    #                 FILTER (STR(?cityName) = '{search_term}')
+    #     }}
+    #     LIMIT 100
+    # """
     # Wikidata Query
     # sparql_query = f"""
     # PREFIX wd: <http://www.wikidata.org/entity/>
@@ -67,7 +67,6 @@ def search_graph():
     # }}
     # LIMIT 10
     # """
-    print(sparql_query)
     table_rows = {}
     try:
         sparql = SPARQLWrapper(graph_uri)
@@ -76,7 +75,7 @@ def search_graph():
         results = sparql.query().convert()
         row_count = 0
         sparql_variables = sorted(results["head"]["vars"])
-        print(sparql_variables)
+        print(f"The following variables are returned by the SPARQL query: {sparql_variables}")
         for result in results["results"]["bindings"]:
             new_row = {}
             for variable in sparql_variables:
@@ -85,11 +84,14 @@ def search_graph():
                     new_row[variable] = current_value
             table_rows[row_count] = new_row
             row_count += 1
-        print(table_rows)
+        print(f"The results of the query: {table_rows}")
+        print(f"The SPARQL query executed:\n {sparql_query}")
         return table_rows
     except Exception as e:
-        print(f"Exception: {e}")
-        return table_rows
+        exception_message = f"Exception: {e}"
+        print(exception_message)
+        return exception_message
+
 
 
 if __name__ == '__main__':
