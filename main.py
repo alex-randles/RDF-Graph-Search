03ceef105c6273e652d397c155d39a5ea7599564
Alex Randles <alexrandles0@gmail.com>
2023-09-28 01:31:39 +0100

    backup - basic functionality working

--- main.py
+++ main.py
@@ -21,17 +21,47 @@ def index():
 def search_graph():
     search_term = request.form.get("search_term")
     graph_uri = request.form.get("graph_uri")
+    # graph_uri = "https://dbpedia.org/sparql"
     print(search_term, graph_uri)
     sparql_query = f"""
     PREFIX crm: <http://erlangen-crm.org/current/>
-    SELECT DISTINCT ?subject ?nameLabel
+    SELECT DISTINCT ?person ?occupation
     WHERE {{
-      ?subject ?predicate ?object; 
+      ?person ?predicate ?object; 
           crm:P1_is_identified_by ?name;
           a crm:E21_Person .
+      ?occupation crm:P107_has_current_or_former_member ?person ;
+                  rdfs:label ?occupationName   .
       ?name rdfs:label ?nameLabel . 
-      FILTER CONTAINS(LCASE(?nameLabel),'{search_term}')     
+      FILTER CONTAINS(LCASE(?occupationName),'{search_term}')     
      }}
+    LIMIT 10000
+    """
+    # sparql_query = f"""
+    #     PREFIX dbo: <http://dbpedia.org/ontology/>
+    #     SELECT ?athlete ?place
+    #     WHERE
+    #     {{
+    #       ?athlete  rdfs:label      ?label ;
+    #                 dbo:birthPlace  ?place .
+    #       ?place    a               dbo:City ;
+    #                 rdfs:label      ?cityName .
+    #                 FILTER (STR(?cityName) = '{search_term}')
+    #     }}
+    #     LIMIT 1000
+    # """
+    sparql_query = """
+    SELECT DISTINCT ?item ?itemLabel ?occupationLabel
+    WHERE 
+    {
+          ?item wdt:P31 wd:Q5;         
+              wdt:P19/wdt:P131* wd:Q60; 
+              wdt:P27 wd:Q30; 
+              wdt:P106 ?occupation;
+              rdfs:label ?itemLabel . 
+         ?occupation wdt:P3321 ?occupationLabel  . 
+         FILTER(LANG(?itemLabel) = "en" && LANG(?occupationLabel) = "en")
+    }
     LIMIT 1000
     """
     table_rows = {}
@@ -46,8 +76,9 @@ def search_graph():
         for result in results["results"]["bindings"]:
             new_row = {}
             for variable in sparql_variables:
-                current_value = result.get(variable).get("value")
-                new_row[variable] = current_value
+                if result.get(variable):
+                    current_value = result.get(variable).get("value")
+                    new_row[variable] = current_value
             table_rows[row_count] = new_row
             row_count += 1
         print(table_rows)
